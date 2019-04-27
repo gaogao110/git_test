2da3bf9c9a8a435995281049bfd9ea827150616e
yuan <1141102206@qq.com>
2019-04-27 16:54:21 +0800

    second test dev branch

--- src/main/java/com/gaogao/test/controller/Test01.java
+++ src/main/java/com/gaogao/test/controller/Test01.java
@@ -3,5 +3,6 @@ package com.gaogao.test.controller;
 public class Test01 {
     public static void main(String[] args) {
         System.out.println("11111111111111111");
+        System.out.println("test Master merge");
     }
 }
---
+++ src/main/java/com/gaogao/test/controller/TestMasterMerge.java
@@ -0,0 +1,8 @@
+package com.gaogao.test.controller;
+
+public class TestMasterMerge {
+    public static void main(String[] args) {
+        System.out.println("test merge dev branch");
+        System.out.println("thirdly");
+    }
+}
